f069d41127451b4c80b44061bf2207ba766c2bb1
Jonathan Saville <jonathan.saville@mac.com>
2017-06-11 17:18:41 +0100

    Initial Commit

---
+++ WiproTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WiproTask
+//
+//  Created by Jonathan Saville on 11/06/2017.
+//  Copyright © 2017 4rm and function limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
